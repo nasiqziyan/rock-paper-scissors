3c9cb366e970991bb85c172a26a541056ba1bd3b
Nasiq Ziyan <nasiq112@gmail.com>
2022-07-17 21:02:57 +0100

    Update game to play for 5 rounds

--- script.js
+++ script.js
@@ -10,13 +10,6 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-    
-
-    if (!(playerSelection == "Rock" || playerSelection == "Paper" || playerSelection == "Scissors")) {
-        return "Please choose from either 'Rock', 'Paper' or Scissors."
-    }
-
     if (playerSelection == computerSelection) {
         return "It's a tie!";
 
@@ -24,11 +17,53 @@ function playRound(playerSelection, computerSelection) {
              (playerSelection == "Paper" && computerSelection == "Rock") || 
              (playerSelection == "Scissors" && computerSelection == "Paper")) {
 
-            return `You Win! ${playerSelection} beats ${computerSelection}.`;
+            return `You Win! ${playerSelection} beats ${computerSelection}. `;
 
     } else {
-            return  `You Lose! ${computerSelection} beats ${playerSelection}.`
+            return  `You Lose! ${computerSelection} beats ${playerSelection}. `
       }
     
 }
 
+function game() {
+
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        console.log(`Round ${i+1}/5`)
+
+        let playerSelection = prompt("Rock, Paper or Scissors?", "");
+        if (playerSelection === null) {
+            return console.log("Cancelled");
+        }
+
+
+        let computerSelection = getComputerChoice();
+
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+        
+        if (!(playerSelection == "Rock" || playerSelection == "Paper" || playerSelection == "Scissors")) {
+            return "Please choose from either 'Rock', 'Paper' or 'Scissors.'";
+        }
+
+        let playRoundValue = playRound(playerSelection, computerSelection);
+        
+
+        if (playRoundValue == `You Win! ${playerSelection} beats ${computerSelection}. `) {
+            console.log(playRoundValue);
+            playerScore++;
+
+        } else if  (playRoundValue == `You Lose! ${computerSelection} beats ${playerSelection}. `) {
+            console.log(playRoundValue);
+            computerScore++;
+
+        } else console.log(playRoundValue);
+        
+        
+        console.log(`You: ${playerScore}\nComputer: ${computerScore}`)
+        
+        
+        
+    }
+}
