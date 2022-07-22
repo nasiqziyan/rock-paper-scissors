d5c2a93e14544aa7e29b30fcc7469be041f4113e
Nasiq Ziyan <nasiq112@gmail.com>
2022-07-22 22:28:53 +0100

    Add styles and interactivity for 1 round of rps.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Hello World");
+const resultBox = document.querySelector('.resultBox');
 
 
 function getComputerChoice() {
@@ -10,60 +10,81 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     
+    playerSelection = playerSelection.target.textContent; //console.log(this.textContent); equally works to replace this line
+    
+    
+    computerSelection = getComputerChoice();
+    
+
     if (playerSelection == computerSelection) {
-        return "It's a tie!";
+        resultBox.textContent = "It's a tie!";
+        
 
     } else if ((playerSelection == "Rock" && computerSelection == "Scissors") || 
-             (playerSelection == "Paper" && computerSelection == "Rock") || 
-             (playerSelection == "Scissors" && computerSelection == "Paper")) {
+            (playerSelection == "Paper" && computerSelection == "Rock") || 
+            (playerSelection == "Scissors" && computerSelection == "Paper")) {
 
-            return `You Win! ${playerSelection} beats ${computerSelection}. `;
+            resultBox.textContent = `You Win! ${playerSelection} beats ${computerSelection}.`;
 
     } else {
-            return  `You Lose! ${computerSelection} beats ${playerSelection}. `
+            resultBox.textContent = `You Lose! ${computerSelection} beats ${playerSelection}.`;
       }
     
 }
 
-function game() {
+// function game() {
 
-    let playerScore = 0;
-    let computerScore = 0;
+//     let playerScore = 0;
+//     let computerScore = 0;
 
-    for (let i = 0; i < 5; i++) {
-        console.log(`Round ${i+1}/5`)
+//     for (let i = 0; i < 5; i++) {
+//         console.log(`Round ${i+1}/5`)
 
-        let playerSelection = prompt("Rock, Paper or Scissors?", "");
-        if (playerSelection === null) {
-            return console.log("Cancelled");
-        }
+//         let playerSelection = prompt("Rock, Paper or Scissors?", "");
+//         if (playerSelection === null) {
+//             return console.log("Cancelled");
+//         }
 
 
-        let computerSelection = getComputerChoice();
+//         let computerSelection = getComputerChoice();
 
-        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+//         playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
         
-        if (!(playerSelection == "Rock" || playerSelection == "Paper" || playerSelection == "Scissors")) {
-            return "Please choose from either 'Rock', 'Paper' or 'Scissors.'";
-        }
+//         if (!(playerSelection == "Rock" || playerSelection == "Paper" || playerSelection == "Scissors")) {
+//             return "Please choose from either 'Rock', 'Paper' or 'Scissors.'";
+//         }
 
-        let playRoundValue = playRound(playerSelection, computerSelection);
+//         let playRoundValue = playRound(playerSelection, computerSelection);
         
 
-        if (playRoundValue == `You Win! ${playerSelection} beats ${computerSelection}. `) {
-            console.log(playRoundValue);
-            playerScore++;
+//         if (playRoundValue == `You Win! ${playerSelection} beats ${computerSelection}. `) {
+//             console.log(playRoundValue);
+//             playerScore++;
 
-        } else if  (playRoundValue == `You Lose! ${computerSelection} beats ${playerSelection}. `) {
-            console.log(playRoundValue);
-            computerScore++;
+//         } else if  (playRoundValue == `You Lose! ${computerSelection} beats ${playerSelection}. `) {
+//             console.log(playRoundValue);
+//             computerScore++;
 
-        } else console.log(playRoundValue);
-        
-        
-        console.log(`You: ${playerScore}\nComputer: ${computerScore}`)
+//         } else console.log(playRoundValue);
         
         
+//         console.log(`You: ${playerScore}\nComputer: ${computerScore}`)
         
-    }
-}
+        
+        
+//     }
+// }
+
+
+
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+rock.addEventListener('click', playRound);
+paper.addEventListener('click', playRound);
+scissors.addEventListener('click', playRound);
+
+
+
+
